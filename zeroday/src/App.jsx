c57002277a4c97ef811776df34ab8bfe0990c4ad
Navin S <navinzoro7@gmail.com>
2025-12-02 12:06:52 +0530

    Created Student Registration form

---
+++ zeroday/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import HelloWorldComponent from "./Hello-world/Hello_World_Component";
+import StudentForm from "./StudentRegistrationForm/StudentRegistrationForm.jsx"
+
+function App() {
+  return (
+    <StudentForm></StudentForm>
+  )
+}
+
+export default App
---
+++ zeroday/src/Hello-world/Hello_World_Component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {HelloWorldStyle} from "./Hello-World_Style"
+
+const HelloWorldComponent = () => {
+    return <HelloWorldStyle>
+        <h1 className="element">Sri Eshwar</h1>
+    </HelloWorldStyle>
+}
+export default HelloWorldComponent;
