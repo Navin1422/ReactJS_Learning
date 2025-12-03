1bd5483562da1fd5fe57b192887a555996e561a0
Navin S <navinzoro7@gmail.com>
2025-12-03 22:25:38 +0530

    DAY 3 React Learning

--- zeroday/src/App.jsx
+++ zeroday/src/App.jsx
@@ -3,11 +3,14 @@ import FlexBoxComponent from "./flex-box/flex-box-component";
 import HelloWorldComponent from "./Hello-world/Hello_World_Component";
 import StudentForm from "./StudentRegistrationForm/StudentRegistrationForm.jsx"
 import LeadersTomorrowComponent from "./Leaders-Tomorrow/leaders-tomorrow-component.jsx"
+import SymbolOfExcellanceComponent from './Symbol-Of-Excellance/Symbol-Of-Excellance.jsx';
+import PostionComponent from './positioning/position-component.jsx'
+import TrustedbyComponent from "./trusted-by/trusted-by-component";
 
 function App() {
   return (
     <div> 
-      <LeadersTomorrowComponent />
+      <TrustedbyComponent />
     </div>
   );
 }
