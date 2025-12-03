b239590e343c0f34f48a7371666ad3b6fcda4296
Navin S <navinzoro7@gmail.com>
2025-12-03 09:37:07 +0530

    Leaders-Tommorrow

--- zeroday/src/App.jsx
+++ zeroday/src/App.jsx
@@ -2,11 +2,12 @@ import './App.css';
 import FlexBoxComponent from "./flex-box/flex-box-component";
 import HelloWorldComponent from "./Hello-world/Hello_World_Component";
 import StudentForm from "./StudentRegistrationForm/StudentRegistrationForm.jsx"
+import LeadersTomorrowComponent from "./Leaders-Tomorrow/leaders-tomorrow-component.jsx"
 
 function App() {
   return (
     <div> 
-      <HelloWorldComponent />
+      <LeadersTomorrowComponent />
     </div>
   );
 }
